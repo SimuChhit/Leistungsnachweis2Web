fbbd85759f75f12c2deb4782a8e48e4611862fdc
Simon <simon.chhit@edu.teko.ch>
2024-05-15 20:56:22 +0200

    [Major] changing classes and ad media control

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     // Selektiert alle Thumbnail-Items
     const thumbnails = document.querySelectorAll('.thumbnail__item');
 
     // Fügt jedem Thumbnail-Item einen Click-Event-Listener hinzu
     thumbnails.forEach(thumbnail => {
-        thumbnail.addEventListener('click', function() {
+        thumbnail.addEventListener('click', function () {
             // Liest das 'data-target' Attribut des geklickten Thumbnails
             const targetId = this.getAttribute('data-target');
 
             // Selektiert das aktive Carousel-Item und entfernt die Klasse 'active'
-            const activeItem = document.querySelector('.carousel__item.active');
+            const activeItem = document.querySelector('.magazin__item.active');
             if (activeItem) {
                 activeItem.classList.remove('active');
             }
